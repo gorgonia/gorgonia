1376511acec5f189c37cf26ca5278f817be431e2
chewxy <chewxy@gmail.com>
2016-09-20 15:51:43 +1000

    Added asm_vecDiv_avx.s and asm_vecDiv_sse.s for float32. Cleaned up for float64 as well

--- tensor/f32/asm_vecDiv_avx.s
+++ tensor/f32/asm_vecDiv_avx.s
@@ -27,8 +27,8 @@ These are the registers I use to store the relevant information:
 	Y0, Y1 - YMM registers. 
 	X0, X1 - XMM registers.
 
-With regards to VDIVPD and VSUBSD, it turns out that the description of these instructions are:
-	VDIVPD ymm1, ymm2, ymm3: Subtract packed double-precision floating-point values in ymm3/mem from ymm2 and stores result in ymm1.[0]
+With regards to VDIVPS and VSUBSS, it turns out that the description of these instructions are:
+	VDIVPS ymm1, ymm2, ymm3: Subtract packed double-precision floating-point values in ymm3/mem from ymm2 and stores result in ymm1.[0]
 
 The description is written with intel's syntax (in this form: Dest, Src1, Src2). 
 When converting to Go's ASM it becomes: (Src2, Src1, Dest)
@@ -40,9 +40,9 @@ This pseudocode best explains the rather simple assembly:
 
 	loop:
 	for {
-		a[i:i+4*8] /= b[i:i+4*8]
+		a[i:i+8*4] /= b[i:i+8*4]
 		lenA -= 8
-		i += 4*8 // 4 elements, 8 bytes each
+		i += 8 * 4 // 8 elements, 4 bytes each
 		
 		if lenA < 0{
 			break
@@ -50,16 +50,16 @@ This pseudocode best explains the rather simple assembly:
 	}
 
 	remainder4head:
-	lenA += 4
+	lenA += 8
 	if lenA == 0 {
 		return
 	}
 
 	remainder4:
 	for {
-		a[i:i+2*8] /= b[i:i+2*8]
-		lenA -=2
-		i += 2 * 8  // 2 elements, 4 bytes each
+		a[i:i+4*4] /= b[i:i+4*4]
+		lenA -=4
+		i += 4 * 4  // 4 elements, 4 bytes each
 		
 		if lenA < 0{
 			break
@@ -67,7 +67,7 @@ This pseudocode best explains the rather simple assembly:
 	}
 
 	remainder1head:
-	lenA += 2
+	lenA += 4
 	if lenA == 0 {
 		return
 	}
@@ -75,7 +75,7 @@ This pseudocode best explains the rather simple assembly:
 	remainder1:
 	for {
 		a[i] /= b[i]
-		i+=8 // each element is 8 bytes
+		i+=4 // each element is 4 bytes
 		lenA--
 	}
 
@@ -83,12 +83,12 @@ This pseudocode best explains the rather simple assembly:
 
 Citation
 ========
-[0]http://www.felixcloutier.com/x86/DIVPD.html
+[0]http://www.felixcloutier.com/x86/DIVPS.html
 */
 
 #include "textflag.h"
 
-// func vecDiv(a, b []float64)
+// func vecDiv(a, b []float32)
 TEXT ·vecDiv(SB), NOSPLIT, $0
 	MOVQ a_data+0(FP), SI
 	MOVQ b_data+24(FP), DI  		// use destination index register for this
@@ -100,7 +100,7 @@ TEXT ·vecDiv(SB), NOSPLIT, $0
 	CMPQ AX, BX
 	JNE		panic 		// jump to panic if not the same length. TOOD: return bloody errors
 
-	SUBQ $4, AX
+	SUBQ $8, AX
 	JL remainder
 
 	// each ymm register can take up to 4 float64s. 
@@ -109,61 +109,60 @@ TEXT ·vecDiv(SB), NOSPLIT, $0
 
 
 loop:
-	// a[0] to a[3]
-	// VMOVUPD (SI), Y0
-	// VMOVUPD (DI), Y1
-	// VDIVPD Y1, Y0, Y0
-	// VMOVUPD Y0, (SI)
-	BYTE $0xc5; BYTE $0xfd; BYTE $0x10; BYTE $0x06;    // vmovupd (%rsi),%ymm0 
-	BYTE $0xc5; BYTE $0xfd; BYTE $0x10; BYTE $0x0f;    // vmovupd (%rdi),%ymm1 
-	BYTE $0xc5; BYTE $0xfd; BYTE $0x5e; BYTE $0xc1;    // vdivpd %ymm1,%ymm0,%ymm0
-	BYTE $0xc5; BYTE $0xfd; BYTE $0x11; BYTE $0x06;    // vmovupd %ymm0,(%rsi) 
+	// a[0] to a[7]
+	// VMOVUPS (SI), Y0
+	// VMOVUPS (DI), Y1
+	// VDIVPS Y1, Y0, Y0
+	// VMOVUPS Y0, (SI)
+	BYTE $0xc5; BYTE $0xfc; BYTE $0x10; BYTE $0x06;    // vmovups (%rsi),%ymm0
+	BYTE $0xc5; BYTE $0xfc; BYTE $0x10; BYTE $0x0f;    // vmovups (%rdi),%ymm1
+	BYTE $0xc5; BYTE $0xfc; BYTE $0x5e; BYTE $0xc1;    // vdivps %ymm1,%ymm0,%ymm0
+	BYTE $0xc5; BYTE $0xfc; BYTE $0x11; BYTE $0x06;    // vmovups %ymm0,(%rsi)
 
 
 	ADDQ $32, SI
 	ADDQ $32, DI
-	SUBQ $4, AX
-	JGE loop
+	SUBQ $8, AX
+	JGE  loop
 
 remainder:
-	ADDQ 	$4, AX
-	JE 		done
+	ADDQ $8, AX
+	JE   done
 
-	SUBQ 	$2, AX
-	JL 		remainder1head
+	SUBQ $4, AX
+	JL   remainder1head
 
-remainder2:
-	// VMOVUPD (SI), X0
-	// VMOVUPD (DI), X1
-	// VDIVPD X1, X0, X0
-	// VMOVUPD X0, (SI)
-	BYTE $0xc5; BYTE $0xf9; BYTE $0x10; BYTE $0x06;    // vmovupd (%rsi),%xmm0
-	BYTE $0xc5; BYTE $0xf9; BYTE $0x10; BYTE $0x0f;    // vmovupd (%rdi),%xmm1
-	BYTE $0xc5; BYTE $0xf9; BYTE $0x5e; BYTE $0xc1;    // vdivpd %xmm1,%xmm0,%xmm0
-	BYTE $0xc5; BYTE $0xf9; BYTE $0x11; BYTE $0x06;    // vmovupd %xmm0,(%rsi)
+remainder4:
+	// VMOVUPS (SI), X0
+	// VMOVUPS (DI), X1
+	// VDIVPS X1, X0, X0
+	BYTE $0xc5; BYTE $0xf8; BYTE $0x10; BYTE $0x06;    // vmovups (%rsi),%xmm0
+	BYTE $0xc5; BYTE $0xf8; BYTE $0x10; BYTE $0x0f;    // vmovups (%rdi),%xmm1
+	BYTE $0xc5; BYTE $0xf8; BYTE $0x5e; BYTE $0xc1;    // vdivps %xmm1,%xmm0,%xmm0
+	BYTE $0xc5; BYTE $0xf8; BYTE $0x11; BYTE $0x06;    // vmovups %xmm0,(%rsi)
 
 	ADDQ $16, SI
 	ADDQ $16, DI
-	SUBQ $2, AX
-	JGE remainder2
+	SUBQ $4, AX
+	JGE  remainder4
 
 remainder1head:
-	ADDQ $2, AX
-	JE done
+	ADDQ $4, AX
+	JE   done
 
 remainder1:
-	// VMOVSD	(SI), X0
-	// VMOVSD	(DI), X1
-	// VDIVSD	X1, X0, X0
-	// VMOVSD	X0, (SI)
-	BYTE $0xc5; BYTE $0xfb; BYTE $0x10; BYTE $0x06;     // vmovsd (%rsi),%xmm0
-	BYTE $0xc5; BYTE $0xfb; BYTE $0x10; BYTE $0x0f;     // vmovsd (%rdi),%xmm1
-	BYTE $0xc5; BYTE $0xfb; BYTE $0x5e; BYTE $0xc1;     // vdivsd %xmm1,%xmm0,%xmm0
-	BYTE $0xc5; BYTE $0xfb; BYTE $0x11; BYTE $0x06;     // vmovsd %xmm0,(%rsi)
+	// VMOVSS	(SI), X0
+	// VMOVSS	(DI), X1
+	// VDIVSS	X1, X0, X0
+	// VMOVSS	X0, (SI)
+	BYTE $0xc5; BYTE $0xfa; BYTE $0x10; BYTE $0x06;    // vmovss (%rsi),%xmm0
+	BYTE $0xc5; BYTE $0xfa; BYTE $0x10; BYTE $0x0f;    // vmovss (%rdi),%xmm1
+	BYTE $0xc5; BYTE $0xfa; BYTE $0x5e; BYTE $0xc1;    // vdivss %xmm1,%xmm0,%xmm0
+	BYTE $0xc5; BYTE $0xfa; BYTE $0x11; BYTE $0x06;    // vmovss %xmm0,(%rsi)
 
 	// update pointer to the top of the data
-	ADDQ 	$8, SI
-	ADDQ	$8, DI
+	ADDQ 	$4, SI
+	ADDQ	$4, DI
 	
 	DECQ AX
 	JNE 	remainder1
@@ -174,4 +173,5 @@ done:
 panic:
 	CALL 	runtime·panicindex(SB)
 	RET
+
 
--- tensor/f32/asm_vecDiv_sse.s
+++ tensor/f32/asm_vecDiv_sse.s
@@ -13,60 +13,58 @@ TEXT ·vecDiv(SB), 7, $0
 	CMPQ AX, BX
 	JNE panic						// jump to panic if not the same length. TOOD: return bloody errors
 
-	SUBQ $8, AX 					// 4 items or more?
+	SUBQ $16, AX 					// 16 items?
 	JL remainder
 
 loop:
 
 	// a[0]
-	MOVAPD (SI), X0
-	MOVAPD (DI), X1
-	DIVPD X1, X0
-	MOVAPD X0, (SI)
+	MOVAPS (SI), X0
+	MOVAPS (DI), X1
+	DIVPS X1, X0
+	MOVAPS X0, (SI)
 
-	MOVAPD 16(SI), X2
-	MOVAPD 16(DI), X3
-	DIVPD X3, X2
-	MOVAPD X2, 16(SI)
+	MOVAPS 16(SI), X2
+	MOVAPS 16(DI), X3
+	DIVPS X3, X2
+	MOVAPS X2, 16(SI)
 
-	MOVAPD 32(SI), X4
-	MOVAPD 32(DI), X5
-	DIVPD X5, X4
-	MOVAPD X4, 32(SI)
+	MOVAPS 32(SI), X4
+	MOVAPS 32(DI), X5
+	DIVPS X5, X4
+	MOVAPS X4, 32(SI)
 
-	MOVAPD 48(SI), X6
-	MOVAPD 48(DI), X7
-	DIVPD X7, X6
-	MOVAPD X6, 48(SI)
+	MOVAPS 48(SI), X6
+	MOVAPS 48(DI), X7
+	DIVPS X7, X6
+	MOVAPS X6, 48(SI)
 
-
-
-	// update pointers (4 * 2 * 8) - 2*2 elements each time, each element is 8 bytes
+	// update pointers. 4 registers, 4 elements per register, 4 bytes per element
 	ADDQ $64, SI
 	ADDQ $64, DI
 
-	// start of array is now 4*2 less
-	SUBQ	$8, AX
+	// start of array is now 4*4 less
+	SUBQ	$16, AX
 	JGE		loop
 
 remainder:
-	ADDQ 	$8, AX
+	ADDQ 	$16, AX
 	JE 		done
 
 remainderloop:
 	
 	// copy into the appropriate registers
-	MOVSD 	(SI), X0
-	MOVSD 	(DI), X1
-	DIVSD	X1, X0
+	MOVSS 	(SI), X0
+	MOVSS 	(DI), X1
+	DIVSS	X1, X0
 
 	// save it back
-	MOVSD	X0, (SI)
+	MOVSS	X0, (SI)
 
 
 	// update pointer to the top of the data
-	ADDQ 	$8, SI
-	ADDQ	$8, DI
+	ADDQ 	$4, SI
+	ADDQ	$4, DI
 
 	DECQ 	AX
 	JNE 	remainderloop
