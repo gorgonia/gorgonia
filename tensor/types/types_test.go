31f266ef696ebec500d123781d265325b0151678
chewxy <chewxy@gmail.com>
2016-10-30 14:32:20 +0800

    Added a NormOrder type and assorted tests in preparation of having the norm

---
+++ tensor/types/types_test.go
@@ -0,0 +1,45 @@
+package types
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var normtests = []struct {
+	val   float64
+	valid bool
+}{
+	{float64(FrobeniusNorm()), true},
+	{float64(NuclearNorm()), true},
+	{float64(InfNorm()), true},
+	{float64(NegInfNorm()), true},
+	{0.0, true},
+	{1.0, true},
+	{-1.0, true},
+	{3.14, false},
+	{-3.14, false},
+	{math.Float64frombits(0x7ff8000000000003), false},
+	{math.Float64frombits(0x7ff8000000000003), false},
+	{math.Float64frombits(0x7ff8000000000003), false},
+}
+
+func TestNorms(t *testing.T) {
+	assert := assert.New(t)
+	for _, nts := range normtests {
+		n := NormOrder(nts.val)
+		valid := n.Valid()
+		assert.Equal(nts.valid, valid, "Uh oh. Val: %v should be %v, Got %v instead", nts.val, nts.valid, valid)
+	}
+
+	// test constructors, because why not
+	assert.Equal(NormOrder(1.0), Norm(1))
+	assert.Equal(NormOrder(-1.0), Norm(-1))
+
+	// testing equivalence of floats is a fraught endeavour. Best to tet them
+	assert.True(FrobeniusNorm().IsFrobenius())
+	assert.True(NuclearNorm().IsNuclear())
+	assert.True(InfNorm().IsInf(1))
+	assert.True(NegInfNorm().IsInf(-1))
+}
