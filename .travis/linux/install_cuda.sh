051212b70d8efb7d8fdab2dc77838a264fd5a119
chewxy <chewxy@gmail.com>
2017-02-21 19:23:02 +1100

    Updated travis.yml to install CUDA

---
+++ .travis/linux/install_cuda.sh
@@ -0,0 +1,4 @@
+travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_${CUDA}_amd64.deb
+travis_retry sudo dpkg -i cuda-repo-ubuntu1404_${CUDA}_amd64.deb
+travis_retry sudo apt-get update -qq
+travis_retry sudo apt-get install cuda
