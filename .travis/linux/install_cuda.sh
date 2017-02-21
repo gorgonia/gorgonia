4833fa474ab5c0ff9b7c7ad4b6d228b1cad1b4d7
chewxy <chewxy@gmail.com>
2017-02-21 20:57:42 +1100

    more CUDA/Travis

--- .travis/linux/install_cuda.sh
+++ .travis/linux/install_cuda.sh
@@ -4,7 +4,7 @@ travis_retry sudo apt-get update -qq
 travis retry sudo apt-get upgrade -qq
 export CUDA_APT=${CUDA:0:3}
 export CUDA_APT=${CUDA_APT/./-}
-travis retry sudo apt-get install -f -y nvidia-375 nvidia-375-dev libcuda1-375 nvidia-settings nvidia-opencl-icd-375
+travis_retry sudo apt-get install -f -y nvidia-375 nvidia-375-dev libcuda1-375 nvidia-settings nvidia-opencl-icd-375
 travis_retry sudo apt-get install -f -y cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT} cuda-cufft-dev-${CUDA_APT}
 travis_retry sudo apt-get install cuda
 travis_retry sudo apt-get clean
