b3423f9212660b1ded35d1e10076a07f2ba7cc69
chewxy <chewxy@gmail.com>
2017-02-21 20:29:22 +1100

    Ugh. This is nuts

--- .travis/linux/install_cuda.sh
+++ .travis/linux/install_cuda.sh
@@ -1,5 +1,8 @@
 travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_${CUDA}_amd64.deb
 travis_retry sudo dpkg -i cuda-repo-ubuntu1404_${CUDA}_amd64.deb
 travis_retry sudo apt-get update -qq
-travis_retry sudo apt-get install -f -y cuda
+export CUDA_APT=${CUDA:0:3}
+export CUDA_APT=${CUDA_APT/./-}
+travis_retry sudo apt-get install -f -y cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT} cuda-cufft-dev-${CUDA_APT}
+travis_retry sudo apt-get install cuda
 travis_retry sudo apt-get clean
