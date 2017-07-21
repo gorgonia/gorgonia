0e4d8652364214428bb90684667899f001239269
chewxy <chewxy@gmail.com>
2017-07-21 21:56:19 +1000

    added incriter for internal engine

--- tensor/genlib2/body.go
+++ tensor/genlib2/body.go
@@ -69,9 +69,9 @@ const (
 		if validi && validj && validk {
 			{{template "check" . -}}
 			{{if .IsFunc -}}
-				{{.Range}}[i] += {{template "callFunc" .}}
+				{{.Range}}[k] += {{template "callFunc" .}}
 			{{else -}}
-				{{.Range}}[i] += {{.Left}} {{template "symbol" .Kind}}  {{.Right}}
+				{{.Range}}[k] += {{.Left}} {{template "symbol" .Kind}}  {{.Right}}
 			{{end -}}		
 		}
 	}`
@@ -128,9 +128,9 @@ const (
 		if validi && validk {
 			{{template "check" . -}}
 			{{if .IsFunc -}}
-				{{.Range}}[i] += {{template "callFunc" .}}
+				{{.Range}}[k] += {{template "callFunc" .}}
 			{{else -}}
-				{{.Range}}[i] += {{.Left}} {{template "symbol" .Kind}}  {{.Right}}
+				{{.Range}}[k] += {{.Left}} {{template "symbol" .Kind}}  {{.Right}}
 			{{end -}}		
 		}
 	}
@@ -175,7 +175,6 @@ const (
 		default:
 		return errors.Errorf("Unsupported type %v for {{$name}}", t)
 	}
-
 	`
 
 	eArithIncrRaw = `as := isScalar(a)
@@ -235,18 +234,54 @@ const (
 	default:
 		return errors.Errorf("Unsupported type %v for {{$name}}", t)
 	}
+	`
 
+	eArithIncrIterRaw = `as :=isScalar(a)
+	bs := isScalar(b)
+	is := isScalar(incr)
+
+	if (as && !bs) || (bs && !as) && is {
+		return errors.Errorf("Cannot increment on a scalar increment. len(a): %d, len(b) %d", a.Len(), b.Len())
+	}
+	{{$name := .Name}}
+	switch t {
+		{{range .Kinds -}}
+	case {{reflectKind .}}:
+		at := a.{{sliceOf .}}
+		bt := b.{{sliceOf .}}
+		it := incr.{{sliceOf .}}
+		switch {
+		case as && bs:
+			{{$name}}{{short .}}(at, bt)
+			if !is {
+				return e.{{$name}}Iter{{short .}}(t, incr, a, iit, ait)
+			}
+			it[0] += at[0]
+			return
+		case as && !bs:
+			return {{$name}}IncrIterSV{{short .}}(at[0], bt, it, bit, iit)
+		case !as && bs:
+			return {{$name}}IncrIterVS{{short .}}(at, bt[0], it, ait, iit)
+		default:
+			return {{$name}}IncrIter{{short .}}(at, bt, it, ait, bit, iit)
+		}
+		{{end -}}
+	default:
+		return errors.Errorf("Unsupported type %v for {{$name}}", t)
+	}
 	`
 )
 
 var (
-	eArith     *template.Template
-	eArithIncr *template.Template
-	eArithIter *template.Template
+	eArith         *template.Template
+	eArithIncr     *template.Template
+	eArithIter     *template.Template
+	eArithIncrIter *template.Template
 )
 
 func init() {
 	eArith = template.Must(template.New("eArith").Funcs(funcs).Parse(eArithRaw))
 	eArithIncr = template.Must(template.New("eArithIncr").Funcs(funcs).Parse(eArithIncrRaw))
 	eArithIter = template.Must(template.New("eArithIter").Funcs(funcs).Parse(eArithIterRaw))
+	eArithIncrIter = template.Must(template.New("eArithIncrIter").Funcs(funcs).Parse(eArithIncrIterRaw))
 }
