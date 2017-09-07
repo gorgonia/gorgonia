0c3c2844af6810133768f03cbaf32c217f7239e5
chewxy <chewxy@gmail.com>
2017-09-07 17:48:20 +1000

    fixed another oopsie

--- tensor/api_unary_test.go
+++ tensor/api_unary_test.go
@@ -772,7 +772,7 @@ func TestLog10(t *testing.T) {
 		if err := typeclassCheck(a.Dtype(), floatTypes); err != nil {
 			return true
 		}
-		ret, err := Log2(a, WithIncr(incr))
+		ret, err := Log10(a, WithIncr(incr))
 		if err, retEarly := qcErrCheck(t, "Log10", a, nil, we, err); retEarly {
 			if err != nil {
 				return false
