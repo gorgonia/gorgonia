3df21230492c812e828363966b9b7d974e73088b
chewxy <chewxy@gmail.com>
2017-09-19 21:19:15 +1000

    Fixed travis

--- .travis/linux/OpenBLAS/test.sh
+++ .travis/linux/OpenBLAS/test.sh
@@ -3,10 +3,6 @@ set -ex
 go env
 
 go test -v -a -covermode=atomic -coverprofile=test.cover .
-go test -v -a -covermode=atomic -coverprofile=./tensor/test.cover ./tensor/
-
-go test -tags='avx' -a  ./tensor/
-go test -tags='sse' -a  ./tensor/
 go test -tags='avx' -a  .
 go test -tags='sse' -a  .
 
