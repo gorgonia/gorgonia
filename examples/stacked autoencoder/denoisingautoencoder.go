c337718b78398c70464ad9fef489bf7767c4c7df
chewxy <chewxy@gmail.com>
2017-09-23 17:02:20 +1000

    Cherry picked the correct files that fixes #160

--- examples/stacked autoencoder/denoisingautoencoder.go
+++ examples/stacked autoencoder/denoisingautoencoder.go
@@ -1,40 +1,24 @@
 package main
 
-import . "github.com/chewxy/gorgonia"
+import (
+	"log"
+
+	. "github.com/chewxy/gorgonia"
+)
 
 type DenoisingAutoencoder struct {
-	*Neuron
+	Neuron
 	LayerConfig
 
-	h  *Neuron
+	h  Neuron
 	af ActivationFunction
 
 	input      *Node
 	corruption *Node
 	corrupted  *Node
 	output     *Node
+	hiddenOut  *Node
 	g          *ExprGraph
-}
-
-func NewDA(corruption float64, opts ...LayerConsOpt) *DenoisingAutoencoder {
-	da := new(DenoisingAutoencoder)
-	da.af = Sigmoid
-	for _, opt := range opts {
-		opt(da)
-	}
-
-	u := func() InitWFn {
-		return GlorotU(1.0)
-	}
-
-	da.Neuron = NewNeuron(da.Inputs, da.Outputs, da.BatchSize, da.g, u)
-	da.h = new(Neuron)
-	da.h.w = Must(Transpose(da.w))
-	da.h.b = NewVector(da.g, Float64, WithShape(da.Outputs), WithInit(Zeroes()))
-
-	da.corruption = BinomialRandomNode(da.g, Float64, 1, corruption)
-
-	return da
 }
 
 func NewDATiedWeights(w, b *Node, corruption float64, opts ...LayerConsOpt) *DenoisingAutoencoder {
@@ -45,24 +29,27 @@ func NewDATiedWeights(w, b *Node, corruption float64, opts ...LayerConsOpt) *Den
 		opt(da)
 	}
 
-	da.Neuron = new(Neuron)
-	da.h = new(Neuron)
 	da.w = w
 	da.b = b
 
-	da.h.w = Must(Transpose(da.w))
+	da.h.w = Must(Transpose(w))
+	log.Printf("w %v da.h.w %v", w.Shape(), da.h.w.Shape())
 	if da.BatchSize == 1 {
-		da.h.b = NewVector(da.g, Float64, WithShape(da.Inputs), WithInit(Zeroes()))
+		da.h.b = NewVector(da.g, dt, WithShape(da.Inputs), WithInit(Zeroes()))
 	} else {
-		da.h.b = NewMatrix(da.g, Float64, WithShape(da.BatchSize, da.Inputs), WithInit(Zeroes()))
+		da.h.b = NewMatrix(da.g, dt, WithShape(da.BatchSize, da.Inputs), WithInit(Zeroes()))
 	}
 
-	da.corruption = BinomialRandomNode(da.g, Float64, 1, corruption)
+	da.corruption = BinomialRandomNode(da.g, dt, 1, corruption)
 
 	return da
 }
 
 func (l *DenoisingAutoencoder) Activate() (retVal *Node, err error) {
+	if l.output != nil {
+		return l.output, nil
+	}
+
 	var xw, xwb *Node
 	if xw, err = Mul(l.corrupted, l.w); err != nil {
 		return
@@ -71,40 +58,57 @@ func (l *DenoisingAutoencoder) Activate() (retVal *Node, err error) {
 	if xwb, err = Add(xw, l.b); err != nil {
 		return
 	}
-	return l.af(xwb)
+
+	if l.output, err = l.af(xwb); err != nil {
+		return nil, err
+	}
+	return l.output, nil
 }
 
-func (l *DenoisingAutoencoder) Reconstruct(y *Node) (retVal *Node, err error) {
+func (l *DenoisingAutoencoder) Reconstruct() (err error) {
+	if l.hiddenOut != nil {
+		return nil
+	}
+
 	var yw, ywb *Node
-	if yw, err = Mul(y, l.h.w); err != nil {
+	if yw, err = Mul(l.output, l.h.w); err != nil {
 		return
 	}
 
 	if ywb, err = Add(yw, l.h.b); err != nil {
 		return
 	}
-	return l.af(ywb)
+
+	if l.hiddenOut, err = l.af(ywb); err != nil {
+		return
+	}
+	return nil
 }
 
-func (l *DenoisingAutoencoder) Corrupt() (*Node, error) {
-	return HadamardProd(l.corruption, l.input)
+func (l *DenoisingAutoencoder) Corrupt() (err error) {
+	if l.corrupted != nil {
+		return nil
+	}
+
+	if l.corrupted, err = HadamardProd(l.corruption, l.input); err != nil {
+		return
+	}
+	return nil
 }
 
 func (l *DenoisingAutoencoder) Cost(x *Node) (retVal *Node, err error) {
-	var hidden, y, loss *Node
-
-	if l.corrupted, err = l.Corrupt(); err != nil {
+	if err = l.Corrupt(); err != nil {
 		return
 	}
-	if hidden, err = l.Activate(); err != nil {
+	if _, err = l.Activate(); err != nil {
 		return
 	}
-
-	if y, err = l.Reconstruct(hidden); err != nil {
+	if err = l.Reconstruct(); err != nil {
 		return
 	}
 
-	if loss, err = BinaryXent(y, l.input); err != nil {
+	var loss *Node
+	if loss, err = BinaryXent(l.hiddenOut, l.input); err != nil {
 		return
 	}
 
