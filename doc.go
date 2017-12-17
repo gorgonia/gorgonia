d5d7ae0a83c14d90903f650a6641ac60c58150b5
chewxy <chewxy@gmail.com>
2017-12-17 14:54:22 +1100

    Updated vanity URL. Ready for merge

--- doc.go
+++ doc.go
@@ -3,4 +3,4 @@ Package gorgonia is a library that helps facilitate machine learning in Go.
 Write and evaluate mathematical equations involving multidimensional arrays easily.
 Do differentiation with them just as easily.
 */
-package gorgonia
+package gorgonia // import "gorgonia.org/gorgonia"
