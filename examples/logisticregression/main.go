48b92ad5a44dd93e88f84fc208c46e31cadfa0e7
tantanchen <tantanchen@gmail.com>
2018-01-24 18:01:55 -0500

    documentation
    and questions

--- examples/logisticregression/main.go
+++ examples/logisticregression/main.go
@@ -1,29 +1,25 @@
 package main
 
-// import "C"
-
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"math/rand"
 	"os"
 	"runtime/pprof"
+	"strconv"
 	"time"
 
 	G "gorgonia.org/gorgonia"
 	"gorgonia.org/tensor"
-	// cblas "gonum.org/v1/netlib/blas/netlib"
 )
 
 const (
-	// N     = 400
-	// feats = 784
-
-	N     = 26733
-	feats = 10
-
+	N         = 26733
+	feats     = 10
 	trainIter = 5000
 )
 
@@ -33,15 +29,9 @@ var static = flag.Bool("static", false, "Use static test file")
 var wT tensor.Tensor
 var yT tensor.Tensor
 var xT tensor.Tensor
-
 var Float = tensor.Float64
 
-func handleError(err error) {
-	if err != nil {
-		log.Fatalf("%+v", err)
-	}
-}
-
+// init generates random values for x, w, and y for demo purposes
 func init() {
 	xBacking := tensor.Random(Float, N*feats)
 	wBacking := tensor.Random(Float, feats)
@@ -78,39 +68,36 @@ func main() {
 		yT = tensor.New(tensor.WithBacking(yBacking), tensor.WithShape(N))
 		wT = tensor.New(tensor.WithBacking(wBacking), tensor.WithShape(feats))
 	}
-
-	// G.Use(cblas.Implementation{})
-	// G.UseNonStable()
-
+	// create a new graph and add x, y, w, b, and one as Nodes
 	g := G.NewGraph()
 	x := G.NewMatrix(g, Float, G.WithName("x"), G.WithShape(N, feats))
 	y := G.NewVector(g, Float, G.WithName("y"), G.WithShape(N))
 
 	w := G.NewVector(g, Float, G.WithName("w"), G.WithShape(feats))
 	b := G.NewScalar(g, Float, G.WithName("bias"))
-
+	// Why do we need a node with the constant one?
 	one := G.NewConstant(1.0)
 
-	// prob = 1/(1 + e^(-(x·w)-b))
-	xwmb := G.Must(G.Add(G.Must(G.Mul(x, w)), b))
+	// create a node that has the operation: (x*w + b)
+	xwmb := G.Must(G.Add(G.Must(G.Mul(x, w)), b)) // since we know we have a matrix*vector, why do we call Mul() here?
+	// create a node that has the operation: sigmoid(xwmb)
 	prob := G.Must(G.Sigmoid(xwmb))
-	G.WithName("prob")(prob)
+	G.WithName("prob")(prob) // why do we assign the name on a separate line? x, y, w, and b were named in one line
 
-	// "forward"
+	// create a "pred" node that has the operation (prod > 0.5)
+	// this ensures that our prediction output is {0,1}
 	pred := G.Must(G.Gt(prob, G.NewConstant(0.5), false))
 	G.WithName("pred")(pred)
 
 	// binary cross entropy: -y * log(prob) - (1-y)*log(1-prob)
 	logProb := G.Must(G.Log(prob))
 	fstTerm := G.Must(G.HadamardProd(G.Must(G.Neg(y)), logProb))
-
 	oneMinusY := G.Must(G.Sub(one, y))
 	logOneMinusProb := G.Must(G.Log(G.Must(G.Sub(one, prob))))
 	sndTerm := G.Must(G.HadamardProd(oneMinusY, logOneMinusProb))
 
 	crossEntropy := G.Must(G.Sub(fstTerm, sndTerm))
 	G.WithName("crossEntropy")(crossEntropy)
-
 	loss := G.Must(G.Mean(crossEntropy))
 
 	// regularization term
@@ -136,7 +123,7 @@ func main() {
 	wUpd := G.Must(G.Sub(w, gwlr))
 	gblr := G.Must(G.Mul(learnRate, grads[1]))
 	bUpd := G.Must(G.Sub(b, gblr))
-
+	// why do we set w to wUpd here? doesn't the set need to one of the instructions in the program after the gradients are applied?
 	G.Set(w, wUpd)
 	G.Set(b, bUpd)
 
@@ -173,7 +160,9 @@ func main() {
 	fmt.Printf("Final Model: \nw: %3.3s\nb: %+3.3s\n", w.Value(), b.Value())
 
 	fmt.Printf("Target values: %#v\n", yT)
+	fmt.Printf("START\n")
 	prog, locMap, err = G.CompileFunction(g, G.Nodes{x}, G.Nodes{pred})
+	fmt.Printf("%+v", err)
 	handleError(err)
 	machine = G.NewTapeMachine(g, G.WithPrecompiled(prog, locMap))
 
@@ -182,4 +171,89 @@ func main() {
 	machine.Let(x, xT)
 	handleError(machine.RunAll())
 	fmt.Printf("Predicted: %#v\n", pred.Value())
+}
+func loadStatic() (w, x, y []float64) {
+	d0, err := os.Open("testdata/X_ds1.10.csv")
+	if err == nil {
+		r := csv.NewReader(d0)
+		r.Comma = ','
+		for {
+			record, err := r.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for _, f := range record {
+				fl, _ := strconv.ParseFloat(f, 64)
+				x = append(x, fl)
+			}
+		}
+		if len(x) != N*feats {
+			log.Fatalf("Expected %d*%d. Got %d instead", N, feats, len(x))
+		}
+	} else {
+		log.Println("could not read from file")
+		x = tensor.Random(Float, N*feats).([]float64)
+	}
+
+	w0, err := os.Open("testdata/W_ds1.10.csv")
+	if err == nil {
+		r := csv.NewReader(w0)
+		r.Comma = ' '
+		for {
+			record, err := r.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fl, _ := strconv.ParseFloat(record[0], 64)
+			w = append(w, fl)
+		}
+
+		if len(w) != feats {
+			log.Fatalf("Expected %d rows. Got %d instead", feats, len(w))
+		}
+	} else {
+		w = tensor.Random(Float, feats).([]float64)
+	}
+
+	y0, err := os.Open("testdata/Y_ds1.10.csv")
+	if err == nil {
+		r := csv.NewReader(y0)
+		r.Comma = ','
+		for {
+			record, err := r.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fl, _ := strconv.ParseFloat(record[0], 64)
+			y = append(y, fl)
+		}
+		if len(y) != N {
+			log.Fatalf("Expected %d rows. Got %d instead", N, len(y))
+		}
+	} else {
+		y = make([]float64, N)
+		for i := range y {
+			y[i] = float64(rand.Intn(2))
+		}
+
+	}
+	return
+}
+
+func handleError(err error) {
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
 }
