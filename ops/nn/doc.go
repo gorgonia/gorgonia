0b05835903c69cb9d0eef37e6c084ed3ddd32793
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-06-25 11:30:30 +0200

    chore: doc

---
+++ ops/nn/doc.go
@@ -0,0 +1,4 @@
+// Package nnops implements some operators that have both a pure go implementation and a cuda implementation
+// to use the cuda version, assuming that you have the pre-requisites, simply compile or run the code with the `cuda tag`
+//  go run -tags='cuda'
+package nnops
