67c1f654244f1f4ce7bb25e8005647f261fe9f1e
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-09-02 17:16:11 +0200

    chore(typo)

--- batch.go
+++ batch.go
@@ -1,12 +1,12 @@
 package gorgonia
 
-// Batched interface describes ny object that can process batch work
+// Batched interface describes any object that can process batch work
 type Batched interface {
 	WorkAvailable() <-chan struct{}
 	DoWork()
 }
 
-// BatchedBLAS interface describes ny object that can process BLAS work in batch
+// BatchedBLAS interface describes any object that can process BLAS work in batch
 type BatchedBLAS interface {
 	Batched
 	BLAS
