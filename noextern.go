23a9eeab84de5379425cb4acba99e404eb42736b
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-09-03 10:33:23 +0200

    chore(doc)

--- noextern.go
+++ noextern.go
@@ -72,7 +72,7 @@ func (m *ExternMetadata) Transfer(toDev, fromDev Device, v Value, synchronous bo
 	return v, nil
 }
 
-// Reset is a noop function for compatibility with the interface
+// Reset is a noop function for compatibility with the Cuda build
 func (m *ExternMetadata) Reset() {}
 
 // Cleanup cleans up the ancillary allocations made during the calling of batched external device function.
