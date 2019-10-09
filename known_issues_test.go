6a2e3502f44339727df0c007db37cd9886ae8d41
volmedo <vicente.olmedo@gmail.com>
2019-10-09 10:30:30 +0200

    Handle errors idiomatically

--- known_issues_test.go
+++ known_issues_test.go
@@ -25,10 +25,18 @@ func TestIssue182(t *testing.T) {
 	b := NewScalar(g, tensor.Float64)
 
 	xT := tensor.New(tensor.WithBacking([]float64{1, 1, 1}), tensor.WithShape(3))
-	// TODO: Check errors returned by Mul and Grad?
-	y, _ := Mul(x, a)
-	z, _ := Mul(y, b)
-	dz, _ := Grad(z, x)
+	y, err := Mul(x, a)
+	if err != nil {
+		t.Fatal(err)
+	}
+	z, err := Mul(y, b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	dz, err := Grad(z, x)
+	if err != nil {
+		t.Fatal(err)
+	}
 	machine := NewTapeMachine(g)
 	defer machine.Close()
 
@@ -107,11 +115,13 @@ func TestIssue268_im2col(t *testing.T) {
 		18, 21, 22, 23, 0, 0, 0, 41, 42, 43, 46, 47, 48, 0, 0, 0, 17, 18, 19, 22, 23, 24, 0, 0, 0, 42, 43, 44, 47, 48,
 		49, 0, 0, 0, 18, 19, 0, 23, 24, 0, 0, 0, 0, 43, 44, 0, 48, 49, 0, 0, 0, 0,
 	}))
-	// TODO: Check the error returned by Im2Col?
-	y, _ := Im2Col(x, []int{3, 3}, []int{1, 1}, []int{1, 1}, []int{1, 1})
+	y, err := Im2Col(x, []int{3, 3}, []int{1, 1}, []int{1, 1}, []int{1, 1})
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	machine := NewTapeMachine(g)
-	if err := machine.RunAll(); err != nil {
+	if err = machine.RunAll(); err != nil {
 		t.Fatal(err)
 	}
 	assert.Equal(t, yT.Shape(), y.Shape(), "Tensors should be the same")
