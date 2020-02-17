abecd374e30e454787b5f253c28ca000921f0754
Ubuntu <ubuntu@ip-172-31-44-74.ap-southeast-1.compute.internal>
2020-02-17 00:10:48 +0000

    added download.sh for downloading MNIST dataset.

---
+++ examples/mnist/download.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DST=${1:-$GOPATH/src/gorgonia.org/gorgonia/examples/testdata}
+cd "$DST"
+
+curl -O http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+curl -O http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+curl -O http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+curl -O http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+
+gunzip t*-ubyte.gz
