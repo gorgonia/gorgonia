7b0eddf23535fb3b095e16f626f14e13271e52f3
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-04-20 15:07:30 +0900

    Fix path to download files

--- examples/mnist/download.sh
+++ examples/mnist/download.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-DST=${1:-$GOPATH/src/gorgonia.org/gorgonia/examples/testdata}
+set -e
+
+DST=$(cd $(dirname $0)/../testdata/mnist; pwd)
+mkdir -p $DST 2> /dev/null
 cd "$DST"
 
 curl -O http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
@@ -8,4 +11,4 @@ curl -O http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
 curl -O http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
 curl -O http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
 
-gunzip t*-ubyte.gz
+gzip -f -d t*-ubyte.gz
