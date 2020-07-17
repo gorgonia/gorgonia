cc10838d1a6f461bd1dfdf858b9908935c6c7315
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2020-07-17 22:22:16 +0200

    feat: add broadcast test

--- x/vm/pubsub_test.go
+++ x/vm/pubsub_test.go
@@ -134,3 +134,92 @@ func Test_newPubsub(t *testing.T) {
 		})
 	}
 }
+
+func Test_pubsub_run(t *testing.T) {
+	type fields struct {
+		publishers  []<-chan gorgonia.Value
+		subscribers []chan ioValue
+	}
+	type args struct {
+		ctx context.Context
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			"nil channel",
+			fields{},
+			args{
+				context.Background(),
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &pubsub{
+				publishers:  tt.fields.publishers,
+				subscribers: tt.fields.subscribers,
+			}
+			if err := p.run(tt.args.ctx); (err != nil) != tt.wantErr {
+				t.Errorf("pubsub.run() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_broadcast(t *testing.T) {
+	t.Run("context cancel without value", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		cs := make([]chan ioValue, 1)
+		for i := range cs {
+			// The size of the channels buffer controls how far behind the receivers
+			// of the fanOut channels can lag the other channels.
+			cs[i] = make(chan ioValue, 0)
+		}
+		c := make(<-chan ioValue, 0)
+		broadcast(ctx, c, cs)
+		cancel()
+	})
+	t.Run("context cancel without value", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		cs := make([]chan ioValue, 1)
+		for i := range cs {
+			// The size of the channels buffer controls how far behind the receivers
+			// of the fanOut channels can lag the other channels.
+			cs[i] = make(chan ioValue, 0)
+		}
+		c := make(chan ioValue, 0)
+		broadcast(ctx, c, cs)
+		c <- ioValue{}
+		cancel()
+	})
+	t.Run("broadcast ", func(t *testing.T) {
+		fortyTwo := gorgonia.F32(42.0)
+		ctx := context.Background()
+		cs := make([]chan ioValue, 2)
+		for i := range cs {
+			// The size of the channels buffer controls how far behind the receivers
+			// of the fanOut channels can lag the other channels.
+			cs[i] = make(chan ioValue, 0)
+		}
+		c := make(chan ioValue, 0)
+		broadcast(ctx, c, cs)
+		val := ioValue{
+			pos: 0,
+			v:   &fortyTwo,
+		}
+		c <- val
+		v0 := <-cs[0]
+		v1 := <-cs[1]
+		if !reflect.DeepEqual(v0, val) {
+			t.Errorf("broadcast want %v, got %v", val, v0)
+		}
+		if !reflect.DeepEqual(v1, val) {
+			t.Errorf("broadcast want %v, got %v", val, v1)
+		}
+	})
+}
