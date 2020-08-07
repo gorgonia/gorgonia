58272bc585ef212bd0cc5c8412aad9c51b29d8d1
Dimitrii <sexykdi@gmail.com>
2020-08-07 10:40:49 +0300

    layer interface, detections, utils, yolo_layer, network class, yolo operation, empty test for yolo op

--- examples/tiny-yolo-v3-coco/yolov3_tiny.go
+++ examples/tiny-yolo-v3-coco/yolov3_tiny.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"gorgonia.org/gorgonia"
+)
+
+// YOLOv3 YOLOv3 architecture
+type YOLOv3 struct {
+	g                                 *gorgonia.ExprGraph
+	classesNum, boxesPerCell, netSize int
+	out                               []*gorgonia.Node
+	layersInfo                        []string
+}
