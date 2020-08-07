9388e917869389b1864b117a512c116fcd43ab39
Dimitrii <sexykdi@gmail.com>
2020-08-07 10:50:15 +0300

    fill main.go with code

--- examples/tiny-yolo-v3-coco/main.go
+++ examples/tiny-yolo-v3-coco/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"time"
+
+	"gorgonia.org/gorgonia"
+	G "gorgonia.org/gorgonia"
+	"gorgonia.org/tensor"
+)
+
 var (
 	imgWidth       = 416
 	imgHeight      = 416
@@ -14,5 +23,52 @@ var (
 )
 
 func main() {
+	g := G.NewGraph()
 
+	input := gorgonia.NewTensor(g, tensor.Float32, 4, gorgonia.WithShape(1, channels, imgWidth, imgHeight), gorgonia.WithName("input"))
+	model, err := NewYoloV3Tiny(g, input, len(cocoClasses), boxes, leakyCoef, cfg, weights)
+	if err != nil {
+		fmt.Printf("Can't prepare YOLOv3 network due the error: %s\n", err.Error())
+		return
+	}
+	model.Print()
+
+	imgf32, err := GetFloat32Image("data/dog_416x416.jpg", imgHeight, imgWidth)
+	if err != nil {
+		fmt.Printf("Can't read []float32 from image due the error: %s\n", err.Error())
+		return
+	}
+	image := tensor.New(tensor.WithShape(1, channels, imgHeight, imgWidth), tensor.Of(tensor.Float32), tensor.WithBacking(imgf32))
+	err = gorgonia.Let(input, image)
+	if err != nil {
+		fmt.Printf("Can't let input = []float32 due the error: %s\n", err.Error())
+		return
+	}
+
+	tm := G.NewTapeMachine(g)
+	defer tm.Close()
+
+	// Feedforward
+	st := time.Now()
+	if err := tm.RunAll(); err != nil {
+		fmt.Printf("Can't run tape machine due the error: %s\n", err.Error())
+		return
+	}
+	fmt.Println("Feedforwarded in:", time.Since(st))
+
+	// Postprocessing
+	st = time.Now()
+	dets, err := model.ProcessOutput(cocoClasses, scoreThreshold, iouThreshold)
+	if err != nil {
+		fmt.Printf("Can't do postprocessing due error: %s", err.Error())
+		return
+	}
+	fmt.Println("Postprocessed in:", time.Since(st))
+
+	fmt.Println("Detections:")
+	for i := range dets {
+		fmt.Println(dets[i])
+	}
+
+	tm.Reset()
 }
