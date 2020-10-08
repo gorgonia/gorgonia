c564b6cb04253ee345a24f010de25e9a73896c2c
David Cuadrado <dcuadrado@truora.com>
2020-10-07 20:58:05 -0500

    Implement ADOp and SDOp for the sparseMax operation
    It implement the methods necessary to run a backpropagation on this
    operation.

--- op_sparsemax.go
+++ op_sparsemax.go
@@ -13,7 +13,7 @@ import (
 type sparsemaxOp struct {
 }
 
-func newSparsemaxOp(inputShape tensor.Shape) *sparsemaxOp {
+func newSparsemaxOp() *sparsemaxOp {
 	sparsemaxop := &sparsemaxOp{}
 
 	return sparsemaxop
@@ -22,8 +22,7 @@ func newSparsemaxOp(inputShape tensor.Shape) *sparsemaxOp {
 // Sparsemax -  implements the sparsemax operation described here: http://proceedings.mlr.press/v48/martins16.pdf
 // Current implementation only supports float64
 func Sparsemax(x *Node) (*Node, error) {
-	xShape := x.Shape()
-	op := newSparsemaxOp(xShape)
+	op := newSparsemaxOp()
 
 	return ApplyOp(op, x)
 }
@@ -69,7 +68,7 @@ func (op *sparsemaxOp) checkInput(inputs ...Value) (tensor.Tensor, error) {
 	var ok bool
 
 	if in, ok = inputs[0].(tensor.Tensor); !ok {
-		return nil, errors.Errorf("Expected input to be a tensor")
+		return nil, errors.Errorf("Expected input to be a tensor, got %T", inputs[0])
 	}
 
 	if in.Shape().Dims() != 1 {
@@ -79,7 +78,7 @@ func (op *sparsemaxOp) checkInput(inputs ...Value) (tensor.Tensor, error) {
 	return in, nil
 }
 
-func (op *sparsemaxOp) Do(inputs ...Value) (retVal Value, err error) {
+func (op *sparsemaxOp) Do(inputs ...Value) (Value, error) {
 	inputTensor, err := op.checkInput(inputs...)
 	if err != nil {
 		return nil, fmt.Errorf("Can't check Sparsemax input: %w", err)
@@ -123,5 +122,199 @@ func (op *sparsemaxOp) Do(inputs ...Value) (retVal Value, err error) {
 	return tensor.New(tensor.Of(inputTensor.Dtype()), tensor.WithShape(inputTensor.Size()), tensor.WithEngine(inputTensor.Engine()), tensor.WithBacking(output)), nil
 }
 
+// DoDiff calculates the diff and sets its value to the output node. Implementation for ADOp interface.
+func (op *sparsemaxOp) DoDiff(ctx ExecutionContext, inputs Nodes, output *Node) error {
+	if len(inputs) != 2 {
+		return fmt.Errorf("SparsemaxOp.DoDiff needs 2 arguments")
+	}
+
+	odv := output.boundTo.(*dualValue)
+	odvd := odv.Value.(tensor.Tensor)
+	diffOp := &sparsemaxDiffOp{}
+
+	result, err := diffOp.Do(inputs[0].boundTo, inputs[1].boundTo)
+	if err != nil {
+		return err
+	}
+
+	sum, err := odvd.(*tensor.Dense).Add(result.(*tensor.Dense), tensor.UseUnsafe())
+	if err != nil {
+		return err
+	}
+
+	odv.d = sum
+
+	return nil
+}
+
+// SymDiff applies the diff op. Implementation for SDOp interface.
+func (op *sparsemaxOp) SymDiff(inputs Nodes, output, grad *Node) (Nodes, error) {
+	err := checkArity(op, len(inputs))
+	if err != nil {
+		return nil, err
+	}
+
+	t := inputs[0]
+
+	diffOp := &sparsemaxDiffOp{}
+	nodes := make(Nodes, 1)
+
+	nodes[0], err = ApplyOp(diffOp, t, grad)
+
+	return nodes, err
+}
+
+// DiffWRT is an implementation for the SDOp interface
+func (op *sparsemaxOp) DiffWRT(inputs int) []bool {
+	if inputs != 1 {
+		panic(fmt.Sprintf("sparsemax operator only supports one input, got %d instead", inputs))
+	}
+
+	return []bool{true}
+}
+
+type sparsemaxDiffOp struct {
+}
+
+func newSparsemaxOpDiff() *sparsemaxDiffOp {
+	return &sparsemaxDiffOp{}
+}
+
+func (op *sparsemaxDiffOp) Arity() int {
+	return 2
+}
+
+func (op *sparsemaxDiffOp) ReturnsPtr() bool { return false }
+
+func (op *sparsemaxDiffOp) CallsExtern() bool { return false }
+
+func (op *sparsemaxDiffOp) WriteHash(h hash.Hash) {
+	fmt.Fprintf(h, "SparsemaxDiff{}()")
+}
+
+func (op *sparsemaxDiffOp) Hashcode() uint32 { return simpleHash(op) }
+
+func (op *sparsemaxDiffOp) String() string {
+	return fmt.Sprintf("SparsemaxDiff{}()")
+}
+
+func (op *sparsemaxDiffOp) InferShape(inputs ...DimSizer) (tensor.Shape, error) {
+	s := inputs[0].(tensor.Shape).Clone()
+
+	return s, nil
+}
+
+func (op *sparsemaxDiffOp) Type() hm.Type {
+	aType := hm.TypeVariable('a')
+
+	ta := newTensorType(1, aType)
+
+	return hm.NewFnType(ta, ta, ta) // f(float64, float64) float64
+}
+
+func (op *sparsemaxDiffOp) OverwritesInput() int { return -1 }
+
+func (op *sparsemaxDiffOp) checkInput(inputs ...Value) (tensor.Tensor, tensor.Tensor, error) {
+	if err := checkArity(op, len(inputs)); err != nil {
+		return nil, nil, err
+	}
+
+	var (
+		in tensor.Tensor
+
+		gradient tensor.Tensor
+		ok       bool
+	)
+
+	switch t := inputs[0].(type) {
+	case *dualValue:
+		if in, ok = t.Value.(tensor.Tensor); !ok {
+			return nil, nil, errors.Errorf("input should be a tensor, got %T", inputs[0])
+		}
+	case tensor.Tensor:
+		in = t
+	default:
+		return nil, nil, errors.Errorf("input type is not supported, got %T", inputs[0])
+	}
+
+	switch t := inputs[1].(type) {
+	case *dualValue:
+		if gradient, ok = t.Value.(tensor.Tensor); !ok {
+			return nil, nil, errors.Errorf("gradient should be a tensor, got %T", inputs[1])
+		}
+	case tensor.Tensor:
+		gradient = t
+	default:
+		return nil, nil, errors.Errorf("gradient type is not supported, got %T", inputs[1])
+	}
+
+	if in.Shape().Dims() != 1 || gradient.Shape().Dims() != 1 {
+		return nil, nil, errors.Errorf("Expected input to have 1 dimensions")
+	}
+
+	return in, gradient, nil
+}
+
+func (op *sparsemaxDiffOp) Do(inputs ...Value) (Value, error) {
+	inputTensor, gradTensor, err := op.checkInput(inputs...)
+	if err != nil {
+		return nil, fmt.Errorf("Can't check SparsemaxDiff input: %w", err)
+	}
+
+	if inputTensor.Size() != gradTensor.Size() {
+		return nil, fmt.Errorf("sparsemaxDiffOp.Do inputs sizes should be equal")
+	}
+
+	data, ok := inputTensor.Data().([]float64)
+	if !ok {
+		return nil, fmt.Errorf("sparsemaxDiffOp.Do expected input to be []float64, got %T", inputTensor.Data())
+	}
+
+	outputData, ok := gradTensor.Data().([]float64)
+	if !ok {
+		return nil, fmt.Errorf("sparsemaxDiffOp.Do expected input to be []float64, got %T", inputTensor.Data())
+	}
+
+	nonZeros := 0.0
+	inputSum := 0.0
+	diff := make([]float64, len(data))
+
+	for i, v := range data {
+		if v == 0.0 {
+			continue
+		}
+
+		diff[i] = 1.0
+
+		inputSum += outputData[i]
+		nonZeros++
+	}
+
+	sum := 0.0
+
+	if nonZeros > 0 {
+		sum = inputSum / nonZeros
+	}
+
+	for i := range diff {
+		diff[i] *= (outputData[i] - sum)
+	}
+
+	val := tensor.New(
+		tensor.Of(inputTensor.Dtype()),
+		tensor.WithShape(inputTensor.Size()),
+		tensor.WithEngine(inputTensor.Engine()),
+		tensor.WithBacking(diff),
+	)
+
+	return val, nil
+}
+
 // ensure it complies with the Op interface
-var _ Op = &sparsemaxOp{}
+var (
+	_ Op = &sparsemaxDiffOp{}
+
+	_ Op   = &sparsemaxOp{}
+	_ SDOp = &sparsemaxOp{}
+	_ ADOp = &sparsemaxOp{}
+)
