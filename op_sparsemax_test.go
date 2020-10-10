9fdf854d00eb8167d3d74f3a8acd5e7f3ee30856
David Cuadrado <krawek@gmail.com>
2020-10-10 14:01:04 -0500

    Implement ADOp and SDOp for the sparseMax operation (#438)
    It implement the methods necessary to run a backpropagation on this
    operation.
    
    Co-authored-by: David Cuadrado <dcuadrado@truora.com>

--- op_sparsemax_test.go
+++ op_sparsemax_test.go
@@ -7,7 +7,7 @@ import (
 	"gorgonia.org/tensor"
 )
 
-var testCases = []struct {
+var testCasesSparseMaxDo = []struct {
 	input    []float64
 	expected []float64
 }{
@@ -25,12 +25,40 @@ var testCases = []struct {
 	},
 }
 
+var testCasesSparseMaxDoDiff = []struct {
+	input []float64
+	grad  []float64
+
+	expected []float64
+}{
+	{
+		[]float64{0.0000, 0.0000, 0.0521, 0.2354, 0.7124},
+		[]float64{0.2860, -0.0702, 0.8080, 0.9913, 1.4683},
+		[]float64{0, 0, -0.2811999999999999, -0.09789999999999999, 0.3791},
+	},
+	{
+		[]float64{0.0556, 0.0000, 0.7118, 0.2325, 0.0000},
+		[]float64{0.1109, -1.4741, 0.7671, 0.2878, 0.0334},
+		[]float64{-0.2777, -0.0000, 0.3785, -0.1008, -0.0000},
+	},
+	{
+		[]float64{0.2841, 0.0000, 0.7159, 0.0000, 0.0000},
+		[]float64{0.2094, -1.0000, 0.6411, -0.5032, -0.3909},
+		[]float64{-0.21585000000000001, 0, 0.21585, 0, 0},
+	},
+	{
+		[]float64{0.2592, 0.0000, 0.6909, 0.0498, 0.0000},
+		[]float64{0.2094, -1.0000, 0.6411, 0.0000, -0.3909},
+		[]float64{-0.07410000000000003, 0, 0.3576, -0.28350000000000003, 0},
+	},
+}
+
 func TestSparsemaxDo(t *testing.T) {
 	c := require.New(t)
 
-	for i, testCase := range testCases {
+	for i, testCase := range testCasesSparseMaxDo {
 		tt := tensor.New(tensor.Of(tensor.Float64), tensor.WithShape(len(testCase.input)), tensor.WithBacking(testCase.input))
-		op := newSparsemaxOp(tt.Shape())
+		op := newSparsemaxOp()
 
 		out, err := op.Do(tt)
 		c.NoError(err, "failed test case: %d", i)
@@ -38,10 +66,73 @@ func TestSparsemaxDo(t *testing.T) {
 	}
 }
 
+func TestSparsemaxDoDiff(t *testing.T) {
+	c := require.New(t)
+
+	for i, testCase := range testCasesSparseMaxDoDiff {
+		g := NewGraph()
+		a := NewTensor(g, Float64, 1, WithName("a"), WithShape(1))
+		b := NewTensor(g, Float64, 1, WithName("b"), WithShape(1))
+
+		op := newSparsemaxOp()
+		r, err := ApplyOp(op, a)
+		c.NoError(err)
+
+		aT := tensor.New(tensor.WithShape(len(testCase.input)), tensor.WithBacking(testCase.input))
+		bT := tensor.New(tensor.WithShape(len(testCase.grad)), tensor.WithBacking(testCase.grad))
+		rT := tensor.New(tensor.WithShape(len(testCase.input)), tensor.WithBacking(make([]float64, len(testCase.grad))))
+
+		aVal, _, _, _ := anyToValue(aT)
+		bVal, _, _, _ := anyToValue(bT)
+		rVal, _, _, _ := anyToValue(rT)
+
+		a.bind(dvUnit(aVal))
+		b.bind(dvUnit(bVal))
+		r.bind(dvUnitVar(rVal))
+
+		err = op.DoDiff(ExecutionContext{}, Nodes{a, b}, r)
+		c.NoError(err, "failed test case: %d", i)
+
+		c.Equal(testCase.expected, r.boundTo.Data())
+	}
+}
+
+func TestSparsemaxDoSymDiff(t *testing.T) {
+	c := require.New(t)
+
+	for i, testCase := range testCasesSparseMaxDoDiff {
+		g := NewGraph()
+		a := NewTensor(g, Float64, 1, WithName("a"), WithShape(1))
+		b := NewTensor(g, Float64, 1, WithName("b"), WithShape(1))
+
+		aT := tensor.New(tensor.WithShape(len(testCase.input)), tensor.WithBacking(testCase.input))
+		bT := tensor.New(tensor.WithShape(len(testCase.grad)), tensor.WithBacking(testCase.grad))
+
+		aVal, _, _, _ := anyToValue(aT)
+		bVal, _, _, _ := anyToValue(bT)
+
+		a.bind(dvUnit(aVal))
+		b.bind(dvUnit(bVal))
+
+		op := newSparsemaxOp()
+		diff, err := op.SymDiff(Nodes{a}, nil, b)
+		c.NoError(err, "failed test case: %d", i)
+
+		c.Len(diff, 1)
+
+		vm := NewTapeMachine(g)
+
+		c.NoError(vm.RunAll())
+		c.NoError(vm.Close())
+
+		c.Equal(testCase.expected, diff[0].boundTo.Data())
+	}
+}
+
 func TestSparsemaxFull(t *testing.T) {
 	c := require.New(t)
 
-	for i, testCase := range testCases {
+	for i, testCase := range testCasesSparseMaxDo {
 		tt := tensor.New(tensor.Of(tensor.Float64), tensor.WithShape(len(testCase.input)), tensor.WithBacking(testCase.input))
 		expected := tensor.New(tensor.Of(tensor.Float64), tensor.WithShape(len(testCase.expected)), tensor.WithBacking(testCase.expected))
 
