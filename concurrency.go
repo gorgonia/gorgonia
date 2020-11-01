25a4bfd6c9605b0f966b32df3c8deaba63ef5c87
chewxy <chewxy@gmail.com>
2020-11-02 09:41:05 +1100

    Fixed calcBlocks

--- concurrency.go
+++ concurrency.go
@@ -18,5 +18,7 @@ const (
 // 	<- ch
 func workersChan() chan struct{} { return make(chan struct{}, runtime.GOMAXPROCS(0)) }
 
-// calcConcurrentBlocks calculuates the best number of blocks given a blocksize
-func calcConcurrentBlocks(dim, blocksize int) int { return (dim + blocksize - 1) / blocksize }
+// it's just a generic ceiling function. Added here to avoid mixing with any potential ceilInt operation
+func calcBlocks(n, maxThreads int) int {
+	return (n + maxThreads - 1) / maxThreads
+}
