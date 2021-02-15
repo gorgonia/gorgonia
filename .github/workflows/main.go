b0a36eda8787c268f4094d188a05ec237a733080
Olivier Wulveryck <owulveryck@users.noreply.github.com>
2021-02-15 09:05:16 +0100

    chore(ci): New layout of the CI (#473)
    * chore(wip): trying to refactor the actions
    
    * chore: removed extra go setup
    
    * chore: build system
    
    * chore(wip): one more test
    
    * chore: refacto
    
    * fix: s/build/go build/
    
    * chore(wip): validate workflow
    
    * feat: new layout
    
    * feat: mark avx and sse as experimental on arm
    
    * feat: change the status badge
    
    * feat: trying codecov
    
    * fix:  only cover the main package for  now
    
    * feat: switch  the badge  to use codecov
    
    * feat: removing race detector for macos and selfrunner
    
    * feat: generator for the workflow
    
    * feat(wip): generate file
    
    * feat: files are generated
    
    * fix: missing space in the test CL
    
    * chore: removed unecessary PATH setting

---
+++ .github/workflows/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+	"text/template"
+)
+
+const (
+	latestGo   = "1.15.x"
+	previousGo = "1.14.x"
+)
+
+func main() {
+	workflowLinux, err := os.OpenFile("runner-github-ubuntu-amd64.yml", os.O_RDWR|os.O_CREATE, 0660)
+	if err != nil {
+		panic(err)
+	}
+	defer workflowLinux.Close()
+	workflowMac, err := os.OpenFile("runner-github-macos-amd64.yml", os.O_RDWR|os.O_CREATE, 0660)
+	if err != nil {
+		panic(err)
+	}
+	defer workflowMac.Close()
+	workflowSelf, err := os.OpenFile("runner-self-hosted.yml", os.O_RDWR|os.O_CREATE, 0660)
+	if err != nil {
+		panic(err)
+	}
+	defer workflowSelf.Close()
+
+	err = generateWorkflow(workflowLinux, "Build and Tests on Linux/amd64", "ubuntu-latest", map[string]bool{
+		"none": false,
+		"avx":  true,
+		"sse":  false,
+	}, true)
+	if err != nil {
+		panic(err)
+	}
+	err = generateWorkflow(workflowMac, "Build and Tests on MacOS/amd64", "macos-latest", map[string]bool{
+		"none": false,
+		"avx":  true,
+		"sse":  true,
+	}, false)
+	if err != nil {
+		panic(err)
+	}
+	err = generateWorkflow(workflowSelf, "Build and Tests on Self-Hosted (arm)", "self-hosted", map[string]bool{
+		"none": true,
+	}, false)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func generateWorkflow(w io.Writer, workflowName, runsOn string, tags map[string]bool, withRace bool) error {
+	tmpl, err := template.New("workflow").Funcs(template.FuncMap{"mapToList": mapToList}).Parse(workflowTmpl)
+	if err != nil {
+		panic(err)
+	}
+
+	return tmpl.Execute(w, workflow{
+		WorkflowName: workflowName,
+		Jobs: []job{
+			{
+				JobID:     "stable-go",
+				JobName:   "Build and test on latest stable Go release",
+				RunsOn:    runsOn,
+				GoVersion: latestGo,
+				Tags:      tags,
+				WithRace:  withRace,
+			},
+			{
+				JobID:     "previous-go",
+				JobName:   "Build and test on previous stable Go release",
+				RunsOn:    runsOn,
+				GoVersion: previousGo,
+				Tags:      tags,
+				WithRace:  withRace,
+			},
+		},
+	})
+}
+
+func mapToList(m map[string]bool) string {
+	var b strings.Builder
+	for tag := range m {
+		b.WriteString(tag)
+		b.WriteString(",")
+	}
+	s := b.String()   // no copying
+	s = s[:b.Len()-1] // no copying (removes trailing ", ")
+	return s
+
+}
