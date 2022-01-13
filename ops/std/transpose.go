d008dde19e238be3bbcd8561d20dc91b93946b9d
chewxy <chewxy@gmail.com>
2022-01-14 08:52:49 +1100

    Added DoDiff for transposeOp

--- ops/std/transpose.go
+++ ops/std/transpose.go
@@ -7,11 +7,13 @@ import (
 	"runtime/trace"
 
 	"github.com/chewxy/hm"
+	"github.com/pkg/errors"
 	"gorgonia.org/gorgonia/exprgraph"
 	gctx "gorgonia.org/gorgonia/internal/context"
 	"gorgonia.org/gorgonia/internal/encoding"
 	"gorgonia.org/gorgonia/types"
 	"gorgonia.org/gorgonia/values"
+	"gorgonia.org/gorgonia/values/dual"
 	"gorgonia.org/shapes"
 	"gorgonia.org/tensor"
 )
@@ -87,6 +89,17 @@ func (op transposeOp) String() string {
 	return buf.String()
 }
 
+/* DIFFERENTIATION */
+
+// diffAxes computes the backwards pass transposition pattern.
+func (op transposeOp) diffAxes() shapes.Axes {
+	newPattern := make(shapes.Axes, len(op.pattern))
+	for i, p := range op.pattern {
+		newPattern[p] = shapes.Axis(i)
+	}
+	return newPattern
+}
+
 /* transposeOp implements symdiff.Op */
 
 // DiffWRT returns []bool{true}.
@@ -94,14 +107,40 @@ func (op transposeOp) DiffWRT(i int) []bool { return []bool{true} }
 
 // SymDiff performs the symbolic differentiation of `transposeOp`.
 func (op transposeOp) SymDiff(g *exprgraph.Graph, inputs []*exprgraph.Node, output, grad *exprgraph.Node) (retVal []*exprgraph.Node, err error) {
-	newPattern := make(shapes.Axes, len(op.pattern))
-	for i, p := range op.pattern {
-		newPattern[p] = shapes.Axis(i)
-	}
+	newPattern := op.diffAxes()
 	op2 := transposeOp{pattern: newPattern}
 	retVal = make([]*exprgraph.Node, 1)
 	if retVal[0], err = apply(g, op2, grN(inputs[0]), grad); err == nil {
 		setGroup(g, encoding.GradientCluster, retVal...)
 	}
 	return
+}
+
+/* transposeOp implements ADOp */
+
+// DoDiff allows transposeOp to be automatically differentiated.
+func (op transposeOp) DoDiff(ctx context.Context, inputs []Tensor, output Tensor) (err error) {
+	newPattern := op.diffAxes()
+
+	adv := exprgraph.T2T(inputs[0]).(*dual.Dual)
+	bdv := exprgraph.T2T(output).(*dual.Dual)
+	advd := adv.Deriv()
+	bdvd := bdv.Deriv()
+
+	// set up new context
+	ctx2, task := trace.NewTask(ctx, op.String())
+
+	op2 := transposeOp{pattern: newPattern}
+
+	d, err := op2.Do(ctx2, bdvd)
+	if err != nil {
+		return errors.Wrap(err, "Failed to perform transposeOp.DoDiff()")
+	}
+	// get an addOp
+	add := Add(adv, d)
+	if advd, err = add.PreallocDo(ctx2, advd, advd, d); err != nil {
+		return errors.Wrap(err, "Failed to perform addition of gradients in transposeOp.DoDiff()")
+	}
+	task.End()
+	return nil
 }
