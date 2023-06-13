c6f9f71fd990d929e79732863a78079ac6d5f2eb
zhenghaoz <zhangzhenghao@hotmail.com>
2023-06-13 21:14:18 +0800

    implement embeddingOp

--- op_embedding.go
+++ op_embedding.go
@@ -3,6 +3,7 @@ package gorgonia
 import (
 	"fmt"
 	"hash"
+	"hash/fnv"
 	"log"
 
 	"github.com/chewxy/hm"
@@ -13,22 +14,22 @@ import (
 type embeddingOp struct {
 }
 
-func (op embeddingOp) Arity() int {
+func (op *embeddingOp) Arity() int {
 	return 2
 }
 
-func (op embeddingOp) Type() hm.Type {
+func (op *embeddingOp) Type() hm.Type {
 	a := hm.TypeVariable('a')
-	return hm.NewFnType(a, a) // f(float64, int64) float64
+	return hm.NewFnType(a, a, a)
 }
 
-func (op embeddingOp) InferShape(inputs ...DimSizer) (tensor.Shape, error) {
+func (op *embeddingOp) InferShape(inputs ...DimSizer) (tensor.Shape, error) {
 	if len(inputs) != 2 {
-		return nil, fmt.Errorf("Embedding requires 2 inputs, got %d", len(inputs))
+		return nil, fmt.Errorf("embedding requires 2 inputs, got %d", len(inputs))
 	}
 	embeddingShape := inputs[0].(tensor.Shape)
 	if embeddingShape.Dims() != 2 {
-		return nil, fmt.Errorf("Embedding requires a 2D embedding matrix, got %dD", embeddingShape.Dims())
+		return nil, fmt.Errorf("embedding requires a 2D embedding matrix, got %dD", embeddingShape.Dims())
 	}
 	inputShape := inputs[1].(tensor.Shape)
 	outputShape := []int{}
@@ -37,18 +38,31 @@ func (op embeddingOp) InferShape(inputs ...DimSizer) (tensor.Shape, error) {
 	return outputShape, nil
 }
 
-func (op embeddingOp) Do(value ...Value) (Value, error) {
+func (op *embeddingOp) Do(value ...Value) (Value, error) {
 	embddingTensor, inputTensor, err := op.checkInput(value...)
 	if err != nil {
-		return nil, fmt.Errorf("Can't check Embedding input: %w", err)
+		return nil, fmt.Errorf("can't check Embedding input: %w", err)
 	}
 	outputShape, err := op.InferShape(embddingTensor.Shape(), inputTensor.Shape())
 	if err != nil {
-		return nil, fmt.Errorf("Can't infer Embedding output shape: %w", err)
+		return nil, fmt.Errorf("can't infer Embedding output shape: %w", err)
 	}
 	ret := tensor.New(tensor.WithShape(outputShape...), tensor.Of(embddingTensor.Dtype()))
 	op.do(embddingTensor.Data(), inputTensor.Data(), ret.Data(), outputShape[len(outputShape)-1])
 	return ret, nil
+}
+
+func (op *embeddingOp) UsePreallocDo(prealloc Value, value ...Value) (Value, error) {
+	embddingTensor, inputTensor, err := op.checkInput(value...)
+	if err != nil {
+		return nil, fmt.Errorf("can't check Embedding input: %w", err)
+	}
+	outputShape, err := op.InferShape(embddingTensor.Shape(), inputTensor.Shape())
+	if err != nil {
+		return nil, fmt.Errorf("can't infer Embedding output shape: %w", err)
+	}
+	op.do(embddingTensor.Data(), inputTensor.Data(), prealloc.Data(), outputShape[len(outputShape)-1])
+	return prealloc, nil
 }
 
 func (op *embeddingOp) checkInput(inputs ...Value) (tensor.Tensor, tensor.Tensor, error) {
@@ -73,18 +87,21 @@ func (op *embeddingOp) checkInput(inputs ...Value) (tensor.Tensor, tensor.Tensor
 }
 
 func (op *embeddingOp) do(embedding, imput, output interface{}, embSize int) {
-	inputV, ok := imput.([]int32)
-	if !ok {
-		log.Panicf("Expected input to be a []int32, got %T", imput)
-	}
-
 	switch v := embedding.(type) {
 	case []float32:
+		inputV, ok := imput.([]float32)
+		if !ok {
+			log.Panicf("Expected input to be a []int32, got %T", imput)
+		}
 		outputV := output.([]float32)
 		for i, d := range inputV {
 			copy(outputV[i*embSize:(i+1)*embSize], v[int(d)*embSize:int(d+1)*embSize])
 		}
 	case []float64:
+		inputV, ok := imput.([]float64)
+		if !ok {
+			log.Panicf("Expected input to be a []int32, got %T", imput)
+		}
 		outputV := output.([]float64)
 		for i, d := range inputV {
 			copy(outputV[i*embSize:(i+1)*embSize], v[int(d)*embSize:int(d+1)*embSize])
@@ -133,11 +150,11 @@ func (op *embeddingOp) DoDiff(ctx ExecutionContext, inputs Nodes, output *Node) 
 }
 
 func (op *embeddingOp) DiffWRT(inputs int) []bool {
-	if inputs != 1 {
-		panic(fmt.Sprintf("dropout operator only supports one input, got %d instead", inputs))
+	if inputs != 2 {
+		panic(fmt.Sprintf("embedding operator only supports two input, got %d instead", inputs))
 	}
 
-	return []bool{true}
+	return []bool{true, false}
 }
 
 func (op *embeddingOp) SymDiff(inputs Nodes, output, grad *Node) (Nodes, error) {
@@ -146,16 +163,16 @@ func (op *embeddingOp) SymDiff(inputs Nodes, output, grad *Node) (Nodes, error) 
 		return nil, err
 	}
 
-	// input := inputs[0]
-	// diff := &dropoutDiffOp{op}
+	weights := inputs[0]
+	indices := inputs[1]
+	diff := &embeddingDiffOp{op}
 
-	// ret, err := ApplyOp(diff, input, output, grad)
-	// if err != nil {
-	// 	return nil, err
-	// }
+	ret, err := ApplyOp(diff, weights, indices, output, grad)
+	if err != nil {
+		return nil, err
+	}
 
-	// return Nodes{ret}, nil
-	return nil, nil
+	return Nodes{ret, nil}, nil
 }
 
 func (op *embeddingOp) SetTraining(isTraining bool) error { return nil }
@@ -164,7 +181,90 @@ type embeddingDiffOp struct {
 	*embeddingOp
 }
 
-// ensure it complies with the Op interface
+func (op *embeddingDiffOp) Arity() int {
+	return 4
+}
+
+func (op *embeddingDiffOp) Type() hm.Type {
+	t := hm.TypeVariable('a')
+	return hm.NewFnType(t, t, t, t, t)
+}
+
+func (op *embeddingDiffOp) InferShape(ds ...DimSizer) (tensor.Shape, error) {
+	return ds[0].(tensor.Shape), nil
+}
+
+func (op *embeddingDiffOp) Do(values ...Value) (Value, error) {
+	weights := values[0].(*tensor.Dense)
+	indices := values[1].(*tensor.Dense)
+	output := values[2].(*tensor.Dense)
+	grad := values[3].(*tensor.Dense)
+
+	dy := ZeroValue(weights)
+	v, err := op.UsePreallocDo(dy, weights, indices, output, grad)
+
+	return v, err
+}
+
+func (op *embeddingDiffOp) UsePreallocDo(prealloc Value, inputs ...Value) (retVal Value, err error) {
+	weights := inputs[0].(*tensor.Dense)
+	indices := inputs[1].(*tensor.Dense)
+	outGrad := inputs[3].(*tensor.Dense)
+	result := prealloc.(*tensor.Dense)
+	embSize := weights.Shape()[1]
+
+	switch weights.Dtype() {
+	case Float64:
+		indicesA := indices.Float64s()
+		dy := outGrad.Float64s()
+		dx := result.Float64s()
+
+		for i, index := range indicesA {
+			for j := 0; j < embSize; j++ {
+				dx[int(index)*embSize+j] = dy[i*embSize+j]
+			}
+		}
+	case Float32:
+		indicesA := indices.Float32s()
+		dy := outGrad.Float32s()
+		dx := result.Float32s()
+
+		for i, index := range indicesA {
+			for j := 0; j < embSize; j++ {
+				dx[int(index)*embSize+j] = dy[i*embSize+j]
+			}
+		}
+	}
+
+	return prealloc, nil
+}
+
+func (op embeddingDiffOp) ReturnsPtr() bool {
+	return true
+}
+
+func (op embeddingDiffOp) CallsExtern() bool {
+	return false
+}
+
+func (op *embeddingDiffOp) OverwritesInput() int {
+	return -1
+}
+
+func (op *embeddingDiffOp) WriteHash(h hash.Hash) {
+	h.Write([]byte(op.String()))
+}
+
+func (op *embeddingDiffOp) Hashcode() uint32 {
+	h := fnv.New32a()
+	op.WriteHash(h)
+	return h.Sum32()
+}
+
+func (op *embeddingDiffOp) String() string {
+	return "embeddingDiffOp"
+}
+
 var (
 	_ Op          = &embeddingOp{}
 	_ ADOp        = &embeddingOp{}
--- op_embedding_test.go
+++ op_embedding_test.go
@@ -2,56 +2,67 @@ package gorgonia
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"gorgonia.org/tensor"
 )
 
 func TestEmbedding(t *testing.T) {
 	var tests = []struct {
 		dt                tensor.Dtype
+		weight            interface{}
 		expected          interface{}
 		expectedGrad      interface{}
 		expectedInputGrad interface{}
 	}{
-		{Float64, []float64{1.0, 1.0, 1.0, 1.0, 1.0}, []float64{0.2, 0.2, 0.2, 0.2, 0.2}, []float64{0, 0, 0, 0, 0}},
-		{Float64, []float64{1.25, 1.25, 1.25, 0.0, 0.0}, []float64{0.2, 0.2, 0.2, 0.2, 0.2}, []float64{1, 1, 1, 0.0, 0.0}},
-		{Float64, []float64{2.0, 2.0, 0.0, 0.0, 0.0}, []float64{0.2, 0.2, 0.2, 0.2, 0.2}, []float64{0.4, 0.4, 0, 0, 0}},
-		{Float32, []float32{1.25, 1.25, 1.25, 0.0, 0.0}, []float32{0.2, 0.2, 0.2, 0.2, 0.2}, []float32{1, 1, 1, 0, 0}},
-		{Float32, []float32{2.0, 2.0, 0.0, 0.0, 0.0}, []float32{0.2, 0.2, 0.2, 0.2, 0.2}, []float32{0.4, 0.4, 0, 0, 0}},
+		{
+			Float64,
+			[]float64{0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5},
+			[]float64{0, 0.5, 2, 2.5, 4, 4.5, 6, 6.5, 8, 8.5},
+			[]float64{0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1},
+			[]float64{0.1, 0.1, 0, 0, 0.1, 0.1, 0, 0, 0.1, 0.1, 0, 0, 0.1, 0.1, 0, 0, 0.1, 0.1, 0, 0},
+		},
+		{
+			Float32,
+			[]float32{0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5},
+			[]float32{0, 0.5, 2, 2.5, 4, 4.5, 6, 6.5, 8, 8.5},
+			[]float32{0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1},
+			[]float32{0.1, 0.1, 0, 0, 0.1, 0.1, 0, 0, 0.1, 0.1, 0, 0, 0.1, 0.1, 0, 0, 0.1, 0.1, 0, 0},
+		},
 	}
 
 	for _, tt := range tests {
 		name := fmt.Sprintf("%v", tt.dt)
 		t.Run(name, func(t *testing.T) {
 			g := NewGraph()
-			emb := NewMatrix(g, tt.dt, WithShape(10, 2), WithName("x"), WithInit(RangedFrom(0)))
-			x := NewVector(g, Int64, WithShape(5), WithName("x"), WithInit(RangedFromWithStep(0, 2)))
+			weight := NewMatrix(g, tt.dt, WithName("weight"), WithValue(tensor.New(tensor.WithShape(10, 2), tensor.WithBacking(tt.weight))))
+			indices := NewVector(g, tt.dt, WithName("indices"), WithShape(5), WithInit(RangedFromWithStep(0, 2)))
 
-			y, err := ApplyOp(&embeddingOp{}, emb, x)
+			y, err := ApplyOp(&embeddingOp{}, weight, indices)
 			assert.NoError(t, err)
 
-			_ = y
-			// cost, _ := Mean(y)
-			// if _, err := Grad(cost, x); err != nil {
-			// 	t.Fatal(err)
-			// }
+			cost, _ := Mean(y)
+			if _, err := Grad(cost, weight); err != nil {
+				t.Fatal(err)
+			}
 
-			// m := NewTapeMachine(g, BindDualValues())
-			// defer m.Close()
-			// defer runtime.GC()
+			m := NewTapeMachine(g, BindDualValues())
+			defer m.Close()
+			defer runtime.GC()
 
-			// require.NoError(t, m.RunAll())
-			// assert.Equal(t, tt.expected, y.Value().Data())
+			require.NoError(t, m.RunAll())
+			assert.Equal(t, tt.expected, y.Value().Data())
 
-			// yGrad, err := y.Grad()
-			// require.NoError(t, err)
-			// assert.Equal(t, tt.expectedGrad, yGrad.Data())
+			yGrad, err := y.Grad()
+			require.NoError(t, err)
+			assert.Equal(t, tt.expectedGrad, yGrad.Data())
 
-			// xGrad, err := x.Grad()
-			// require.NoError(t, err)
-			// assert.Equal(t, tt.expectedInputGrad, xGrad.Data())
+			wGrad, err := weight.Grad()
+			require.NoError(t, err)
+			assert.Equal(t, tt.expectedInputGrad, wGrad.Data())
 		})
 	}
 }
