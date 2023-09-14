568d87f2cf7c427814115b83f07a1031a71c0c2c
wzzhu <wenzhang.zhu@gmail.com>
2023-09-14 16:51:08 +0800

    Remove reshape for result matrix in BatchedMatMul if preallocated

--- operatorLinAlg.go
+++ operatorLinAlg.go
@@ -438,6 +438,12 @@ func batchedMatMul(a, b, c tensor.Tensor, transA, transB, incr bool) (retVal ten
 	outer := shapeA[:len(shapeA)-2]
 	innerA := shapeA[len(shapeA)-2:]
 	innerB := shapeB[len(shapeB)-2:]
+	if transA {
+		innerA[0], innerA[1] = innerA[1], innerA[0]
+	}
+	if transB {
+		innerB[0], innerB[1] = innerB[1], innerB[0]
+	}
 
 	if c == nil {
 		newShape := append(outer, innerA[0], innerB[1])
@@ -465,11 +471,9 @@ func batchedMatMul(a, b, c tensor.Tensor, transA, transB, incr bool) (retVal ten
 
 		if transA {
 			as.T()
-			innerA[0], innerA[1] = innerA[1], innerA[0]
 		}
 		if transB {
 			bs.T()
-			innerB[0], innerB[1] = innerB[1], innerB[0]
 		}
 
 		// Reshape the result matrix slice in case matrices like 1x1 will be converted to scalar which results in
@@ -478,10 +482,6 @@ func batchedMatMul(a, b, c tensor.Tensor, transA, transB, incr bool) (retVal ten
 			return nil, err
 		}
 		if err := reshape("b", bs, innerB...); err != nil {
-			return nil, err
-		}
-
-		if err := reshape("c", cs, innerA[0], innerB[1]); err != nil {
 			return nil, err
 		}
 
